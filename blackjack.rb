3c0b84cf281f29122ed45b7cd255240618ade818
Larry Adames <adames.larry@gmail.com>
2014-07-04 23:47:32 -0400

    First Black Jack Game
    This is the first project from Tealeaf Course. Black Jack game in
    procedural form.

---
+++ blackjack.rb
@@ -0,0 +1,19 @@
+cards = {:hearts => [2,3,4,5,6,7,8,9,10, 'J','Q','K','A'], :diamonds => [2,3,4,5,6,7,8,9,10, 'J','Q','K','A'], :spades => [2,3,4,5,6,7,8,9,10, 'J','Q','K','A'], :clubs => [2,3,4,5,6,7,8,9,10, 'J','Q','K','A']}
+def dealer_turn
+
+end
+
+def player_turn
+
+end
+
+def check_win
+
+end
+
+puts "Hello, welcome to Larry's BlackJack Table!!!"
+player_score = 0
+dealer_score = 0
+
+
+
