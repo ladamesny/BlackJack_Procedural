fd8bf1cb7800b893c026303e4a652021eb9de118
Larry Adames <adames.larry@gmail.com>
2014-07-06 22:50:18 -0400

    3rd Revision - name, correct game rules, extra decks etc.
    Following changes were made:
    1. Request user name and use it through out the program
    2. Gave user option to continue playing another round of the game
    3. Added three full card decks to deter card counters
    4. Corrected the game for the following:
    a) Only showed one of the dealer's card without disclosing the score.
    b) Dealer hits up to 17 (not 16)
    5. Made cosmetic changes to the output to make easier to read for the
    user.
    6. Cleaned up the code for redundancy to make code easier to read.

--- blackjack.rb
+++ blackjack.rb
@@ -1,20 +1,44 @@
 require 'pry'
-all_cards = {   :hearts => [2,3,4,5,6,7,8,9,10, 'J','Q','K','A'], 
-			:diamonds => [2,3,4,5,6,7,8,9,10, 'J','Q','K','A'], 
-			:spades => [2,3,4,5,6,7,8,9,10, 'J','Q','K','A'], 
-			:clubs => [2,3,4,5,6,7,8,9,10, 'J','Q','K','A']}
 
-cards_drawn =  { :hearts => [], 
-      :diamonds => [], 
-      :spades => [], 
-      :clubs => []}
+#We are playing with four decks of cards to prevent card counting
+all_cards = { :hearts => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+			        :diamonds => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+			        :spades => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+			        :clubs => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'],
+              :heartsA => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :diamondsA => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :spadesA => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :clubsA => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'],
+              :heartsB => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :diamondsB => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :spadesB => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :clubsB => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'],
+              :heartsC => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :diamondsC => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :spadesC => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace'], 
+              :clubsC => [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace']}
+
+#This hash will store all the cards drawn from the deck
+cards_drawn =  {  :hearts => [], 
+                  :diamonds => [], 
+                  :spades => [], 
+                  :clubs => [],
+                  :heartsA => [], 
+                  :diamondsA => [], 
+                  :spadesA => [], 
+                  :clubsA => [],
+                  :heartsB => [], 
+                  :diamondsB => [], 
+                  :spadesB => [], 
+                  :clubsB => [],
+                  :heartsC => [], 
+                  :diamondsC => [], 
+                  :spadesC => [], 
+                  :clubsC => []}
 
 
-suits = [:hearts, :diamonds,:spades, :clubs ]
-numbers = [2,3,4,5,6,7,8,9,10, 'J','Q','K','A']
-
-suit = suits[rand(0..3)] #this will hold the suit of the drawn card
-number = numbers[rand(0..12)] #this will hold the number of the card(face cards and Aces included)
+suits = [:hearts, :diamonds,:spades, :clubs, :heartsA, :diamondsA,:spadesA, :clubsA ,:heartsB, :diamondsB,:spadesB, :clubsB ,:heartsC, :diamondsC,:spadesC, :clubsC  ]
+numbers = [2,3,4,5,6,7,8,9,10, 'Jack','Queen','King','Ace']
 
 def draw_card suits, suit, numbers, number, cards_drawn, score
   acceptable_card = false
@@ -25,17 +49,32 @@ def draw_card suits, suit, numbers, number, cards_drawn, score
       acceptable_card = true
       return cards_drawn
     else
-      suit = suits[rand(0..3)]
+      suit = suits[rand(0..15)]
       number = numbers[rand(0..12)]
     end
   end  
 end
 
+#This method helps identify the suit among the 4 decks. i.e. "hearts" instead of "heartsA" of "heartsC"
+def card_suit suit
+  if suit =~ /hearts/
+    return "hearts"
+  elsif suit =~ /diamonds/
+    return "diamonds"
+  elsif suit =~ /spades/
+    return "spades"
+  else
+    return "clubs"
+  end
+
+end
+
+
 def update_score score, number
 
-  if number == 'J' ||number == 'Q'||number == 'K'
+  if number == 'Jack' ||number == 'Queen'||number == 'King'
     score += 10
-  elsif number == 'A'
+  elsif number == 'Ace'
     if score < 11
       score += 11
     else
@@ -46,173 +85,195 @@ def update_score score, number
   end
 
 end
-
-
     
-player_score = 0
-dealer_score = 0
-puts
-puts
-puts "Hello, Welcome to Lucky Larry's BlackJack....feeling lucky?"
-puts "Press [ENTER] to continue..."
-gets
+puts ("\n\n               Hello, Welcome to...").center(50)
 puts ("-"*50).center(50)
-puts "Player's Turn".center(50)
+puts ("\n          LUCKY LARRY'S BLACKJACK TABLE!").center(50)
 puts ("-"*50).center(50)
-puts "Press [ENTER] to continue..."
+gets
+puts "\nWhat is your name?"
+name = gets.chomp
+puts "\nHi " + name +"! Let's play some BlackJack!"
+puts "Press [ENTER] to continue...\n"
 gets
 
-#set conditions to beginning setting
-hit = true
-bust = false
-blackjack = false
-dealer_bust = false
-
-#Draw Player's first card
-suit = suits[rand(0..3)]
-number = numbers[rand(0..12)]
-cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, player_score
-player_score = update_score player_score, number
-temp1 = suit
-temp2 = number
-#Draw Player's second card
-suit = suits[rand(0..3)]
-number = numbers[rand(0..12)]
-cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, player_score
-player_score = update_score player_score, number
-puts "You were delt a #{temp2} of #{temp1} and a #{number} of #{suit} for a total score of #{player_score}!"
-puts "Press [ENTER] to continue..."
-gets
-puts
-
-if player_score == 21
-    puts 'BLACKJACK'
-    puts "Press [ENTER] to continue..."
-    gets
-    blackjack = true
-end
 
 
-while (hit == true) && (bust == false) && (blackjack == false) do
+keep_playing = true
+
+while keep_playing == true
   
-  if player_score < 22 && blackjack == false
-    puts
-    puts "Would you like to hit?(y/n)"
-    answer = gets.chomp
-    if answer != 'y'
-      hit = false
-    else
-      suit = suits[rand(0..3)]
-      number = numbers[rand(0..12)]
-      cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, player_score
-      player_score = update_score player_score, number
-      puts "You drew a #{number} of #{suit} for a total score of #{player_score}!"
+  #set conditions to default settings for each new game
+  hit = true
+  bust = false
+  blackjack = false
+  dealer_bust = false
+  player_score = 0
+  dealer_score = 0
+
+  puts ("-"*50).center(50)
+  puts (name + "'s Turn").center(50)
+  puts ("-"*50).center(50)
+  puts "\nPress [ENTER] to continue...\n"
+  gets
+  #Draw Player's first card
+  suit = suits[rand(0..15)]
+  number = numbers[rand(0..12)]
+  cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, player_score
+  player_score = update_score player_score, number
+  temp1 = card_suit suit
+  temp2 = number
+  #Draw Player's second card
+  suit = suits[rand(0..15)]
+  number = numbers[rand(0..12)]
+  cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, player_score
+  player_score = update_score player_score, number
+  suit = card_suit suit
+  puts name + " was delt a #{temp2} of #{temp1} and a #{number} of #{suit} for a total score of #{player_score}!"
+  puts "\nPress [ENTER] to continue...\n"
+  gets
+
+  if player_score == 21
+      puts "BLACKJACK".center(50)
       puts "Press [ENTER] to continue..."
       gets
-    end
-
-    
-  else
-    puts "YOU BUSTED!"
-    puts "Press [ENTER] to continue..."
-    gets
-    hit = false
-    bust = true
-
+      blackjack = true
   end
-
-end
 
 if blackjack == false && bust == false
   #dealer moves next
-  dealer_blackjack = false
-  puts ("-"*50).center(50)
-  puts "Dealer's Turn".center(50)
-  puts ("-"*50).center(50)
-  puts "Press [ENTER] to continue..."
-  gets
+    dealer_blackjack = false
+    puts ("-"*50).center(50)
+    puts "Dealer's Turn".center(50)
+    puts ("-"*50).center(50)
+    puts "\nPress [ENTER] to continue..."
+    gets
 
-  suit = suits[rand(0..3)]
-  number = numbers[rand(0..12)]
-  cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, dealer_score
-  dealer_score = update_score dealer_score, number
-  temp1 = suit
-  temp2 = number
-  suit = suits[rand(0..3)]
-  number = numbers[rand(0..12)]
-  cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, dealer_score
-  dealer_score = update_score dealer_score, number
-  puts ("-"*50).center(50)
-  puts "Dealer drew a #{temp2} of #{temp1} and a #{number} of #{suit} for a total score of #{dealer_score}!"
-  puts "Press [ENTER] to continue..."
-  gets
-end
+    suit = suits[rand(0..15)]
+    number = numbers[rand(0..12)]
+    cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, dealer_score
+    dealer_score = update_score dealer_score, number
+    suit = suits[rand(0..15)]
+    number = numbers[rand(0..12)]
+    cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, dealer_score
+    dealer_score = update_score dealer_score, number
+    suit = card_suit suit
+    if dealer_score ==21
+      dealer_blackjack = true
+      puts "BLACKJACK by the dealer"
+    else
+      puts "\nDealer drew two cards. The dealer is showing a #{number} of #{suit}."
+    end
+    puts "\nPress [ENTER] to continue..."
+    gets
+  end
 
-if dealer_score ==21
-    dealer_blackjack = true
-end
-
-while ((dealer_score <17)  && (blackjack == false) && (dealer_bust == false) && (bust == false) && (dealer_blackjack == false)) do
-
+  if blackjack == false && dealer_blackjack == false
+    puts ("-"*50).center(50)
+    puts (name +"'s turn again").center(50)
+    puts ("-"*50).center(50)
+  end
   
-  suit = suits[rand(0..3)]
-  number = numbers[rand(0..12)]
-  cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, dealer_score
-  dealer_score = update_score dealer_score, number
-  puts ("-"*50).center(50)
-  puts "Dealer drew a #{number} of #{suit} for a total score of #{dealer_score}!"
-  if dealer_score > 21
-    puts "Dealer busts!"
-    dealer_bust = true
-  end
-  puts "Press [ENTER] to continue..."
-  gets
-
-  if dealer_score ==21
-    dealer_blackjack = true
-  end
+  
+  while (hit == true) && (bust == false) && (blackjack == false) && (dealer_blackjack == false) do
+    
+    if player_score < 21 && blackjack == false
+      puts
+      puts "\n \n Would you like to hit?(y/n)"
+      answer = gets.chomp
+      if answer != 'y'
+        hit = false
+      else
+        suit = suits[rand(0..15)]
+        number = numbers[rand(0..12)]
+        cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, player_score
+        player_score = update_score player_score, number
+        suit = card_suit suit
+        puts "You drew a #{number} of #{suit} for a total score of #{player_score}!"
+        puts "Press [ENTER] to continue..."
+        gets
+      end
 
     
+    elsif player_score > 21 && blackjack == false
+      puts (name.upcase + " BUSTED!").center(50)
+      puts "\nPress [ENTER] to continue..."
+      gets
+      hit = false
+      bust = true
+    end
 
-end
-
-
-if ((player_score > dealer_score) && (bust == false) )  || blackjack == true || dealer_bust == true
-  puts "PLAYER WINS!!!!".center(50)
-  if dealer_bust == true
-    puts "Dealer busted!".center(50)
   end
-  puts
-  puts "Press [ENTER] to continue..."
-  gets
-  puts
-  puts "FINAL SCORES".center(50)
-  puts "Player Score: #{player_score}".ljust(50/2) + "Dealer Score: #{dealer_score}".rjust(50/2)
-  puts
-  puts "Thank you for playing!".center(50)
-elsif bust == true || ((player_score < dealer_score) && (player_score < 22) && (dealer_score < 22))
-  puts "Sorry, DEALER WINS...".center(50)
-  if bust == true
-    puts "You busted!".center(50)
+  if (bust == false && blackjack == false && dealer_blackjack == false && dealer_bust == false)
+    puts ("-"*50).center(50)
+    puts "Dealer's Turn again".center(50)
+    puts ("-"*50).center(50)
+    puts "\nPress [ENTER] to continue..."
+    gets
+
+    puts "\nThe dealer turns his card. The dealer's score is : #{dealer_score}"
+    puts "Press [ENTER] to continue..."
+    gets
+  
+    while dealer_score <17 do
+      suit = suits[rand(0..15)]
+      number = numbers[rand(0..12)]
+      cards_drawn = draw_card suits, suit, numbers, number, cards_drawn, dealer_score
+      dealer_score = update_score dealer_score, number
+      suit = card_suit suit
+      puts "Dealer drew a #{number} of #{suit} for a total score of #{dealer_score}!"
+      if dealer_score > 21
+        puts "Dealer busts!"
+        dealer_bust = true
+      end
+      puts "Press [ENTER] to continue..."
+      gets
+
+      if dealer_score ==21
+        dealer_blackjack = true
+      end
+    end
   end
-  puts "Better luck next time!".center(50)
-  puts
-  puts "Press [ENTER] to continue..."
-  gets
-  puts
-  puts "FINAL SCORES".center(50)
-  puts "Player Score: #{player_score}".ljust(50/2) + "Dealer Score: #{dealer_score}".rjust(50/2)
-  puts
-  puts "Thank you for playing!".center(50)
-elsif player_score == dealer_score
-  puts "TIE!!!!!!! --- DEALER WINS...".center(50)
-  puts "Better luck next time!".center(50)
-  puts
-  puts "Press [ENTER] to continue..."
-  gets
-  puts
-  puts "SCORES".center(50)
-  puts "Player Score: #{player_score}".ljust(50/2) + "Dealer Score: #{dealer_score}".rjust(50/2)
-  puts
-  puts "Thank you for playing!".center(50)
+
+
+
+  if ((player_score > dealer_score) && (bust == false) )  || blackjack == true || dealer_bust == true
+    puts (name.upcase+" WINS!!!!").center(50)
+    if dealer_bust == true
+      puts "Dealer busted!".center(50)
+    end
+    puts "\nPress [ENTER] to continue..."
+    gets
+    puts "\nFINAL SCORES".center(50)
+    puts name+"'s Score: #{player_score}".ljust(50/2) + "Dealer Score: #{dealer_score}".rjust(50/2)
+    puts "\nThank you for playing! Would you like to play again? (y/n)".center(50)
+    answer = gets.chomp
+    if answer != 'y'
+      keep_playing = false
+    end
+  elsif bust == true || ((player_score < dealer_score) && (player_score < 22) && (dealer_score < 22))
+    puts "\nSorry, DEALER WINS...".center(50)
+    puts "Better luck next time!".center(50)
+    puts "\nPress [ENTER] to continue..."
+    gets
+    puts "\nFINAL SCORES".center(50)
+    puts name+ "'s Score: #{player_score}".ljust(50/2) + "Dealer Score: #{dealer_score}".rjust(50/2)
+    puts "\nThank you for playing! Would you like to play again? (y/n)".center(50)
+    answer = gets.chomp
+    if answer != 'y'
+      keep_playing = false
+    end
+  elsif player_score == dealer_score
+    puts "\nTIE!!!!!!! --- DEALER WINS...".center(50)
+    puts "Better luck next time!".center(50)
+    puts "\nPress [ENTER] to continue..."
+    gets
+    puts "\nSCORES".center(50)
+    puts name+"'s Score: #{player_score}".ljust(50/2) + "Dealer Score: #{dealer_score}".rjust(50/2)
+    puts "\nThank you for playing! Would you like to play again? (y/n)".center(50)
+    answer = gets.chomp
+    if answer != 'y'
+      keep_playing = false
+    end
+  end
 end
